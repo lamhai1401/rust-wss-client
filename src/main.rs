ede9c47120c214de24e5840d2a58a60428cbc05d
lamhai1401 <lamthanhhai141@gmail.com>
2022-02-10 21:44:39 +0700

    split stream to writer, reader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use futures_util::{SinkExt, StreamExt};
 use log::*;
-use tokio_tungstenite::{
-    connect_async,
-    tungstenite::{Error, Message, Result},
-};
+use tokio::io::{AsyncWriteExt, Result};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use url::Url;
 
 #[tokio::main]
@@ -14,15 +12,31 @@ async fn main() -> Result<()> {
     let (mut socket, _) = connect_async(
         Url::parse("ws://127.0.0.1:8088/ws/?id=123").expect("Can't connect to case count URL"),
     )
-    .await?;
+    .await
+    .expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
 
-    // let _ = socket.write_message(Message::Text(r#"["123", "event", "data", "temp"]"#.into()));
+    let (mut write, read) = socket.split();
 
     let msg = Message::Text(r#"["123", "event", "data", "temp"]"#.into());
-    let _ = socket.send(msg).await?;
 
-    loop {
-        let msg = socket.next().await.expect("Can't fetch case count")?;
-        println!("Received: {}", msg);
-    }
+    println!("sending");
+    // let _ = socket.send(msg).await?;
+
+    write.send(msg).await.unwrap();
+    // loop {
+    //     let msg = socket.next().await.expect("Can't fetch case count")?;
+    //     println!("Received: {}", msg);
+    // }
+
+    let read_future = read.for_each(|message| async {
+        let data = message.unwrap().to_string();
+        // tokio::io::stdout().write(&data).await.unwrap();
+        let msg = format!("received msg from server: {:?}", data);
+        println!("{:}", msg);
+    });
+
+    read_future.await;
+
+    Ok(())
 }
